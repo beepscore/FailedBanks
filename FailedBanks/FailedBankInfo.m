0d7dfa5c7ee1d915115cffc66392f0abd5a8cf93
beepscore <support@beepscore.com>
2012-06-07 22:21:52 -0700

    Add model class FailedBankInfo.

---
+++ FailedBanks/FailedBankInfo.m
@@ -0,0 +1,13 @@
+//
+//  FailedBankInfo.m
+//  FailedBanks
+//
+//  Created by Steve Baker on 6/7/12.
+//  Copyright (c) 2012 Beepscore LLC. All rights reserved.
+//
+
+#import "FailedBankInfo.h"
+
+@implementation FailedBankInfo
+
+@end
