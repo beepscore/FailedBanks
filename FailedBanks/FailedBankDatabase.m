11ef0e4f503f99d367b6c8da4ec68185c8f10d2e
beepscore <support@beepscore.com>
2012-06-07 22:48:47 -0700

    Add class FailedBankDatabase

---
+++ FailedBanks/FailedBankDatabase.m
@@ -0,0 +1,13 @@
+//
+//  FailedBankDatabase.m
+//  FailedBanks
+//
+//  Created by Steve Baker on 6/7/12.
+//  Copyright (c) 2012 Beepscore LLC. All rights reserved.
+//
+
+#import "FailedBankDatabase.h"
+
+@implementation FailedBankDatabase
+
+@end
