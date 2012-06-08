7dfd425174189ed30b5247d8544ea23ab29ecce4
beepscore <support@beepscore.com>
2012-06-07 22:45:34 -0700

    Implement FailedBankInfo

--- FailedBanks/FailedBankInfo.h
+++ FailedBanks/FailedBankInfo.h
@@ -8,6 +8,21 @@
 
 #import <Foundation/Foundation.h>
 
-@interface FailedBankInfo : NSObject
+@interface FailedBankInfo : NSObject {
+    int _uniqueId;
+    NSString *_name;
+    NSString *_city;
+    NSString *_state;
+}
+
+@property (nonatomic, assign) int uniqueId;
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSString *city;
+@property (nonatomic, copy) NSString *state;
+
+- (id)initWithUniqueId:(int)uniqueId 
+                  name:(NSString *)name 
+                  city:(NSString *)city 
+                 state:(NSString *)state;
 
 @end
--- FailedBanks/FailedBankInfo.m
+++ FailedBanks/FailedBankInfo.m
@@ -10,4 +10,28 @@
 
 @implementation FailedBankInfo
 
+@synthesize uniqueId = _uniqueId;
+@synthesize name = _name;
+@synthesize city = _city;
+@synthesize state = _state;
+
+- (id)initWithUniqueId:(int)uniqueId 
+                  name:(NSString *)name 
+                  city:(NSString *)city 
+                 state:(NSString *)state {
+    if ((self = [super init])) {
+        self.uniqueId = uniqueId;
+        self.name = name;
+        self.city = city;
+        self.state = state;
+    }
+    return self;
+}
+
+- (void) dealloc {
+    self.name = nil;
+    self.city = nil;
+    self.state = nil;    
+}
+
 @end
