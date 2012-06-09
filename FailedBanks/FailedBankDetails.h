63fe4b77c726f6a4c7164a6a4ffe19830c401348
beepscore <support@beepscore.com>
2012-06-09 00:52:37 -0700

    Implement FailedBankDetails. Change closeDate to closingDate.

--- FailedBanks/FailedBankDetails.h
+++ FailedBanks/FailedBankDetails.h
@@ -6,8 +6,34 @@
 //  Copyright (c) 2012 Beepscore LLC. All rights reserved.
 //
 
+
 #import <Foundation/Foundation.h>
 
-@interface FailedBankDetails : NSObject
+@interface FailedBankDetails : NSObject {
+    int _uniqueId;
+    NSString *_name;
+    NSString *_city;
+    NSString *_state;
+    int _zip;
+    NSDate *_closingDate;
+    NSDate *_updatedDate;
+}
+
+@property (nonatomic, assign) int uniqueId;
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSString *city;
+@property (nonatomic, copy) NSString *state;
+@property (nonatomic, assign) int zip;
+@property (nonatomic, retain) NSDate *closingDate;
+@property (nonatomic, retain) NSDate *updatedDate;
+
+- (id)initWithUniqueId:(int)uniqueId 
+                  name:(NSString *)name 
+                  city:(NSString *)city 
+                 state:(NSString *)state 
+                   zip:(int)zip 
+           closingDate:(NSDate *)closingDate 
+           updatedDate:(NSDate *)updatedDate;
 
 @end
+
--- FailedBanks/FailedBankDetails.m
+++ FailedBanks/FailedBankDetails.m
@@ -9,5 +9,40 @@
 #import "FailedBankDetails.h"
 
 @implementation FailedBankDetails
+@synthesize uniqueId = _uniqueId;
+@synthesize name = _name;
+@synthesize city = _city;
+@synthesize state = _state;
+@synthesize zip = _zip;
+@synthesize closingDate = _closingDate;
+@synthesize updatedDate = _updatedDate;
+
+- (id)initWithUniqueId:(int)uniqueId name:(NSString *)name 
+                  city:(NSString *)city 
+                 state:(NSString *)state 
+                   zip:(int)zip 
+           closingDate:(NSDate *)closingDate 
+           updatedDate:(NSDate *)updatedDate {
+    
+    if ((self = [super init])) {
+        self.uniqueId = uniqueId;
+        self.name = name;
+        self.city = city;
+        self.state = state;
+        self.zip = zip;
+        self.closingDate = closingDate;
+        self.updatedDate = updatedDate;
+    }
+    return self;
+}
+
+- (void) dealloc
+{
+    self.name = nil;
+    self.city = nil;
+    self.state = nil;
+    self.closingDate = nil;
+    self.updatedDate = nil;
+}
 
 @end
