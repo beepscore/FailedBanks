1f99cf2a55e050780135509c452fd3e41cbdf482
beepscore <support@beepscore.com>
2012-06-09 00:38:11 -0700

    Add FailedBankDetails.

---
+++ FailedBanks/FailedBankDetails.h
@@ -0,0 +1,13 @@
+//
+//  FailedBankDetails.h
+//  FailedBanks
+//
+//  Created by Steve Baker on 6/9/12.
+//  Copyright (c) 2012 Beepscore LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FailedBankDetails : NSObject
+
+@end
---
+++ FailedBanks/FailedBankDetails.m
@@ -0,0 +1,13 @@
+//
+//  FailedBankDetails.m
+//  FailedBanks
+//
+//  Created by Steve Baker on 6/9/12.
+//  Copyright (c) 2012 Beepscore LLC. All rights reserved.
+//
+
+#import "FailedBankDetails.h"
+
+@implementation FailedBankDetails
+
+@end
