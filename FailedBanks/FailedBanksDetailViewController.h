7a11dc50fdd115b7652962cc10dd2947c57eb062
beepscore <support@beepscore.com>
2012-06-10 14:59:50 -0700

    Implement FailedBankDetailViewController.

--- FailedBanks/FailedBanksDetailViewController.h
+++ FailedBanks/FailedBanksDetailViewController.h
@@ -8,6 +8,22 @@
 
 #import <UIKit/UIKit.h>
 
-@interface FailedBanksDetailViewController : UIViewController
+@interface FailedBanksDetailViewController : UIViewController {
+    UILabel *_nameLabel;
+    UILabel *_cityLabel;
+    UILabel *_stateLabel;
+    UILabel *_zipLabel;
+    UILabel *_closedLabel;
+    UILabel *_updatedLabel;
+    int _uniqueId;
+}
+
+@property (nonatomic, retain) IBOutlet UILabel *nameLabel;
+@property (nonatomic, retain) IBOutlet UILabel *cityLabel;
+@property (nonatomic, retain) IBOutlet UILabel *stateLabel;
+@property (nonatomic, retain) IBOutlet UILabel *zipLabel;
+@property (nonatomic, retain) IBOutlet UILabel *closingDateLabel;
+@property (nonatomic, retain) IBOutlet UILabel *updatedLabel;
+@property (nonatomic, assign) int uniqueId;
 
 @end
--- FailedBanks/FailedBanksDetailViewController.m
+++ FailedBanks/FailedBanksDetailViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "FailedBanksDetailViewController.h"
+#import "FailedBankDatabase.h"
+#import "FailedBankDetails.h"
 
 @interface FailedBanksDetailViewController ()
 
@@ -14,6 +16,21 @@
 
 @implementation FailedBanksDetailViewController
 
+@synthesize nameLabel = _nameLabel;
+@synthesize cityLabel = _cityLabel;
+@synthesize stateLabel = _stateLabel;
+@synthesize zipLabel = _zipLabel;
+@synthesize closingDateLabel = _closingDateLabel;
+@synthesize updatedLabel = _updatedLabel;
+@synthesize uniqueId = _uniqueId;
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+    }
+    return self;
+}
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -29,12 +46,43 @@
     // Do any additional setup after loading the view from its nib.
 }
 
+- (void)viewWillAppear:(BOOL)animated {
+    FailedBankDetails *details = [[FailedBankDatabase database] 
+                                  failedBankDetails:_uniqueId];
+    if (details != nil) {
+        [_nameLabel setText:details.name];
+        [_cityLabel setText:details.city];
+        [_stateLabel setText:details.state];
+        [_zipLabel setText:[NSString stringWithFormat:@"%d", details.name]];
+        NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+        [formatter setDateFormat:@"MMMM dd, yyyy"];
+        [_closingDateLabel setText:[formatter stringFromDate:details.closingDate]];
+        [_updatedLabel setText:[formatter stringFromDate:details.updatedDate]];        
+    }
+}
+
 - (void)viewDidUnload
 {
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
+    self.nameLabel = nil;
+    self.cityLabel = nil;
+    self.stateLabel = nil;
+    self.zipLabel = nil;
+    self.closingDateLabel = nil;
+    self.updatedLabel = nil;
 }
+
+- (void) dealloc {
+    self.nameLabel = nil;
+    self.cityLabel = nil;
+    self.stateLabel = nil;
+    self.zipLabel = nil;
+    self.closingDateLabel = nil;
+    self.updatedLabel = nil;
+}
+
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
